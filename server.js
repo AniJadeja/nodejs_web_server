412d2dc5aeeffdb18608a4531fd22058cd860dd9
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-01-28 11:04:09 -0500

    Express Hello world

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-
+const express = require("express");
+const app = express();
 const path = require("path");
-
 const PORT = process.env.PORT || 3500;
 
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    });
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
