14094e72c0fbb986cfa743d37be5dbb3eb966250
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-01-28 11:12:02 -0500

    Sets up router and 404

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const path = require("path");
 const PORT = process.env.PORT || 3500;
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    });
+app.get("^/$ | index(.html)?", (req, res) => {
+    //the path must start with / and end with / or /index.html
+    // where .html is optional
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+app.get("/new-page(.html)?", (req, res) => {
+  
+    res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+app.get("/old-page(.html)?", (req, res) => {
+  
+    res.redirect( 301, "new-page.html");
+});
 
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+}); 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
